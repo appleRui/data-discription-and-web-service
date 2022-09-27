8c595fa713b37dcd6cff32e725bc03181af6f7d0
appleRui <apple31581@gmail.com>
2022-09-27 15:20:04 +0900

    完了：課題3

--- src/main/java/YahooShoppingShippingExtractor.java
+++ src/main/java/YahooShoppingShippingExtractor.java
@@ -1,5 +1,46 @@
+import java.io.*;
+import java.net.*;
+
 public class YahooShoppingShippingExtractor {
   public static void main(String[] args) {
-    System.out.println("YahooShoppingShippingExtractor");
+    try {
+      URL url = new URL("https://paypaymall.yahoo.co.jp/store/sunnapstore/item/cd-7db/?sc_i=shp_pc_top_history");
+      Webpage page = new Webpage(url);
+      page.printPostage();
+    }
+    catch(MalformedURLException e) {
+      System.err.println("Wrong URL: " + args[0]);
+    }
+  }
+}
+
+class Webpage {
+  private URL url;
+
+  Webpage(URL url) {
+    this.url = url;
+  }
+
+  public void printPostage() {
+    try {
+      URLConnection urlConnection = url.openConnection();
+      urlConnection.connect();
+      BufferedReader fin = new BufferedReader(
+              new InputStreamReader(
+                      urlConnection.getInputStream(), "utf-8"));
+      String line;
+      while((line = fin.readLine()) != null)
+        if(line.contains("送料") && line.contains("円")) {
+          int startIndex = line.indexOf("送料");
+          int endIndex = line.indexOf("円");
+          System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+          System.out.println(line.substring(startIndex, endIndex+1));
+          System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+          return;
+        }
+    }
+    catch(IOException e) {
+      System.err.println("I/O Error: " + e.toString());
+    }
   }
 }
