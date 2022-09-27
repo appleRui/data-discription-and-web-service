007f1e72790fb0f38ed3847acb19961c056c95c7
appleRui <apple31581@gmail.com>
2022-09-27 14:42:04 +0900

    init

---
+++ src/main/java/YahooShoppingShippingExtractor.java
@@ -0,0 +1,5 @@
+public class YahooShoppingShippingExtractor {
+  public static void main(String[] args) {
+    System.out.println("YahooShoppingShippingExtractor");
+  }
+}
---
+++ src/test/java/jp/ac/dendai/im/web/dataDescriptionAndWebService/MyTests.java
@@ -0,0 +1,18 @@
+package jp.ac.dendai.im.web.dataDescriptionAndWebService;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyTests {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyTests.class);
+
+	@Test
+	void testSomething() throws Exception {
+		LOGGER.info("Started tests.");
+		assertTrue(false);
+	}
+}
